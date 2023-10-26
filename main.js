4eb147af66ac52d9a1f4424bbf9b4def52e5bac2
Raimer Thompson <rthomp22@gmail.com>
2023-10-25 21:15:58 -0400

    configuring home pahe image slider js

--- main.js
+++ main.js
@@ -383,32 +383,67 @@ searchBtnClose.addEventListener('click', () => {
 
 
 
-let slideIndex = 1;
-showSlides(slideIndex);
+// let slideIndex = 1;
+// showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   // slides[slideIndex-1].style.display = "block";
+// }
+// ;;
+
+//image slider
+var imageno = 1;
+displayImg(imageno);
+
+function nextImg(n){
+  displayImg(imageno += n)
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide(n){
+  displayImg(imageno = n)
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+function displayImg(n){
+  var i ;
+  var image = document.getElementsByClassName('image');
+  var dots = document.getElementsByClassName('dot')
+
+  if (n > image.length){
+    imageno = 1;
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  if (n < 1){
+    imageno = image.length;
   }
-  // slides[slideIndex-1].style.display = "block";
+
+  for (i = 0; i < image.length; i++){
+    image[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++){
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+
+  image[imageno - 1].style.display = "block";
+  dots[imageno - 1].className += ' active';
 }
-;
 
